373ad084bb125592b427268ff5de51d7a4f5c491
Aravindh Sridharan <mail@aravindh.io>
2026-09-11 12:15:24 +0530

    Release Laminar 0.1.4

--- Formula/laminar.rb
+++ Formula/laminar.rb
@@ -1,9 +1,9 @@
 class Laminar < Formula
   desc "Distributed data engine for query, ETL, and streaming workloads"
   homepage "https://github.com/aravindh-e6x/laminar-homebrew-tap"
-  url "https://github.com/aravindh-e6x/laminar-homebrew-tap/releases/download/v0.1.3/laminar-v0.1.3-aarch64-apple-darwin.tar.gz"
-  version "0.1.3"
-  sha256 "042bfbf85f59216fc4e16bf9787e47287eff95ac103fb1ec1f73db34c5b25c72"
+  url "https://github.com/aravindh-e6x/laminar-homebrew-tap/releases/download/v0.1.4/laminar-v0.1.4-aarch64-apple-darwin.tar.gz"
+  version "0.1.4"
+  sha256 "a1cc852d4d4aadf6313a7b550b4864cf19abecc5f4d38e84bbc21311d09016a3"
   license any_of: ["Apache-2.0", "MIT"]
 
   depends_on :macos
